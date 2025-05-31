b4fcd071fd0176d09acf687cd32b143bd9f31557
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-06-01 00:22:53 +0100

    remove #include <stdint.h> for c++98 compatibity

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,13 +6,15 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 17:55:35 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/31 19:02:37 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/06/01 00:22:17 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <stdint.h>
+
 #include <ostream>
 #include "Data.hpp"
 
