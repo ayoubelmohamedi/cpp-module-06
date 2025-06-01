c95143e42319261feaf1645d717ec443fe5db657
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-06-01 01:08:11 +0100

    use stdint.h instead of <cstdint> for c++98

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,15 +6,13 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 17:55:35 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/06/01 00:22:17 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/06/01 00:24:55by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
 #include <stdint.h>
-
 #include <ostream>
 #include "Data.hpp"
 
