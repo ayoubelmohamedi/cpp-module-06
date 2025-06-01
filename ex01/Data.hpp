fcb347f8aede8bf7e87b6e41d9befbb0cd70bf2f
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-06-01 01:03:11 +0100

    fix definition inside header

--- ex01/Data.hpp
+++ ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 18:05:08 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/31 19:19:07 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/05/31 21:47:10 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ class Data
         Data &operator=(const Data & other);
         ~Data();
 
-        const std::string& getName() const {return (_name);};
-        int getNumber() const {return (_n);};
+        const std::string& getName() const;
+        int getNumber() const;
 
         void setNumber(int nbr);
         void setName(std::string &name);
